01e01ba14bb06347900b4f9aa1ac75ca9fe0d6d2
JockFax32 <foxtjack@gmail.com>
2017-03-04 12:53:11 -0800

    Make season metric

--- data-prep.r
+++ data-prep.r
@@ -18,18 +18,27 @@ data$yr_renovated[data$yr_renovated == 0] <- NA
 data$sqft_living_diff <- data$sqft_living - data$sqft_living15
 data$sqft_lot_diff <- data$sqft_lot - data$sqft_lot15
 
+#Create Seasons
+data$date <- paste0(month,'/', day, '/', year)
+
+# Find the season the home was sold
+year.quarter <- as.yearqtr(as.yearmon(data$date, "%m/%d/%Y") + 1/12)
+data$season <- factor(format(year.quarter, "%q"), levels = 1:4,
+                      labels = c("Winter", "Spring", "Summer", "Fall"))
+
+
 # summary(data)
 
 
 # Optional variables.
 #
 # price.lower.3.quartiles.by.thousands <- data[data$price < 645000,]$price.thousands
-# 
+#
 # sqft_living.lower.3.quartiles <- data[data$sqft_living < 2550,]$sqft_living
-# 
+#
 # flat.data <- melt(data[,-c(2:5)], id.vars = "id")
-# 
+#
 # data.for.price.sqft.plot <- data[data$price < 645000,][data$sqft_living < 2550,]
-# 
+#
 # with.waterfront <- data[data$waterfront == 1,]
 # without.waterfront <- data[data$waterfront == 0,]
