7dd8d339aae166b3ecae35aa5f05eb96bb436710
JockFax32 <foxtjack@gmail.com>
2017-03-04 22:18:54 -0800

    Formatt date

--- data-prep.r
+++ data-prep.r
@@ -1,10 +1,11 @@
 library(reshape2)
 library(zoo)
 
+
 #################
 ### Data Prep ###
 #################
-data <- read.csv('./data/training.csv')
+data <- read.csv('./data/training.csv', stringsAsFactors = FALSE)
 
 # Factorize numeric data points
 data$floors <- factor(data$floors)
@@ -16,23 +17,22 @@ data$yr_built <- factor(data$yr_built)
 data$yr_renovated <- factor(data$yr_renovated)
 data$zipcode <- factor(data$zipcode)
 
-
 ### Create Custom Metrics ##
 data$price.thousands <- data$price / 1000
 data$sqft_basement[data$sqft_basement == 0] <- NA
 data$yr_renovated[data$yr_renovated == 0] <- NA
 data$sqft_living_diff <- data$sqft_living - data$sqft_living15
 data$sqft_lot_diff <- data$sqft_lot - data$sqft_lot15
-data$date <- paste0(month,'/', day, '/', year)
 
 # Find the season the home was sold
+month <- substr(data$date, 5, 6)
+year <- substr(data$date, 1, 4)
+day <- substr(data$date, 7, 8)
+data$date <- paste0(month,'/', day, '/', year)
+
 year.quarter <- as.yearqtr(as.yearmon(data$date, "%m/%d/%Y") + 1/12)
 data$season <- factor(format(year.quarter, "%q"), levels = 1:4,
                       labels = c("Winter", "Spring", "Summer", "Fall"))
-
-
-# summary(data)
-
 
 # Optional variables.
 #
