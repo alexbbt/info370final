a7dacbda5a30d6215eb7af01ce9d5bd1bfdcc0f6
Spencer Pease <spencer.p.816@gmail.com>
2017-03-06 02:06:21 -0800

    Update `analysis.R` to read in models from `modeling.R`

--- analysis.R
+++ analysis.R
@@ -1,17 +1,24 @@
-## Analysis file for housing data
+## Analysis file for predicting housing prices
 #
 
 # Source files
 source('data-prep.r')
+source('modeling.R')
 
 # Needed libraries
-
+library(readr)
+library(reshape2)
+library(tidyr)
+library(ggplot2)
+library(dplyr)
 
 # Load data
-
+test.data <- read_csv('data/testing.csv') %>%
+    data.prep()
 
 
 ################
 ### Analysis ###
 ################
+
 
