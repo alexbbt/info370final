3ed8445cf2fc759e1b63abe2f80f7350271325a2
Spencer Pease <spencer.p.816@gmail.com>
2017-03-05 23:19:45 -0800

    Create Shiny app files

---
+++ shiny_app/ui.R
@@ -0,0 +1,29 @@
+## shiny ui file
+
+# Needed libraries
+library(shiny)
+
+
+#########################
+### Shiny UI function ###
+#########################
+
+shinyUI(fluidPage(
+
+    # Application title
+    titlePanel("Old Faithful Geyser Data"),
+
+    # Sidebar with a slider input for number of bins
+    sidebarLayout(sidebarPanel(
+        sliderInput(
+            "bins",
+            "Number of bins:",
+            min = 1,
+            max = 50,
+            value = 30
+        )
+    ),
+
+    # Show a plot of the generated distribution
+    mainPanel(plotOutput("distPlot")))
+))
