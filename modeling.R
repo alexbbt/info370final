853207734d8ca142e5958ada73f5717b58d1f4a4
Alexander Bell-Towne <alexander@bell-towne.com>
2017-03-07 22:18:50 -0800

    Clean up Models for reusablility

--- modeling.R
+++ modeling.R
@@ -13,87 +13,59 @@ library(tree)
 
 # Read CSVs
 training <- read.csv('./data/training.csv')
-verification <- read.csv('./data/verification.csv')
 
-# Create Custom dfs
-t.df <- data.prep(training)
-v.df <- data.prep(verification)
+
+# Factorize Data frame
+training.data <- data.prep(training)
+
 
 # Ensure that all factorise have the same levels
-grade_levels <- union(levels(t.df$grade), levels(v.df$grade))
-v.df$grade <- with(v.df, factor(grade, levels = grade_levels))
-t.df$grade <- with(t.df, factor(grade, levels = grade_levels))
+grade_levels <- as.character(c(3:13))
+training.data$grade <- with(training.data, factor(grade, levels = grade_levels))
 
-condition_levels <- union(levels(t.df$condition), levels(v.df$condition))
-v.df$condition <- with(v.df, factor(condition, levels = condition_levels))
-t.df$condition <- with(t.df, factor(condition, levels = condition_levels))
+condition_levels <- as.character(c(1:5))
+training.data$condition <- with(training.data, factor(condition, levels = condition_levels))
 
-floors_levels <- union(levels(t.df$floors), levels(v.df$floors))
-v.df$floors <- with(v.df, factor(floors, levels = floors_levels))
-t.df$floors <- with(t.df, factor(floors, levels = floors_levels))
+floors_levels <- as.character(c(2:7)/2)
+training.data$floors <- with(training.data, factor(floors, levels = floors_levels))
 
-view_levels <- union(levels(t.df$view), levels(v.df$view))
-v.df$view <- with(v.df, factor(view, levels = view_levels))
-t.df$view <- with(t.df, factor(view, levels = view_levels))
+view_levels <- as.character(c(0:4))
+training.data$view <- with(training.data, factor(view, levels = view_levels))
 
-yr_renovated_levels <- union(levels(t.df$yr_renovated), levels(v.df$yr_renovated))
-v.df$yr_renovated <- with(v.df, factor(yr_renovated, levels = yr_renovated_levels))
-t.df$yr_renovated <- with(t.df, factor(yr_renovated, levels = yr_renovated_levels))
+yr_renovated_levels <- as.character(c(c(0), c(1934:2015)))
+training.data$yr_renovated <- with(training.data, factor(yr_renovated, levels = yr_renovated_levels))
 
-yr_built_levels <- union(levels(t.df$yr_built), levels(v.df$yr_built))
-v.df$yr_built <- with(v.df, factor(yr_built, levels = yr_built_levels))
-t.df$yr_built <- with(t.df, factor(yr_built, levels = yr_built_levels))
+yr_built_levels <- as.character(c(1900:2015))
+training.data$yr_built <- with(training.data, factor(yr_built, levels = yr_built_levels))
 
-zipcode_levels <- union(levels(t.df$zipcode), levels(v.df$zipcode))
-v.df$zipcode <- with(v.df, factor(zipcode, levels = zipcode_levels))
-t.df$zipcode <- with(t.df, factor(zipcode, levels = zipcode_levels))
+zipcode_levels <- as.character(c(98001:98119))
+training.data$zipcode <- with(training.data, factor(zipcode, levels = zipcode_levels))
+
 
 # Find R Squared of all features
 rsquared <- function(coefficient, feature) {
-  model <- lm(price.thousands ~ coefficient, data = t.df)
+  model <- lm(price.thousands ~ coefficient, data = training.data)
   r.squared <- summary(model)$r.squared
   return(r.squared)
 }
 
-features <- lapply(1:25, function(x) names(t.df)[x])
-r.values <- lapply(1:25, function(x) rsquared(t.df[,x]))
+features <- lapply(1:25, function(x) names(training.data)[x])
+r.values <- lapply(1:25, function(x) rsquared(training.data[,x]))
 rsquared.df <- do.call(rbind, Map(data.frame, 'Feaure' = features, 'R_Squared' = r.values)) %>%
   arrange(-R_Squared)
 
+
 # Prep data for trees by removing character types and factors with less than
 # two levels
-new_t.df <- t.df %>% 
-    select(-price, -season, -date, -waterfront)
-new_v.df <- v.df %>% 
-  select(-price, -season, -date, -waterfront)
+training.data.cleaned <- training.data %>% select(-price, -season, -date, -waterfront)
 
-input.response <- new_v.df$price.thousands
 
 # Run the trees
-fit.all <- ctree(price.thousands ~ ., data = new_t.df)
-pred.response.all <- predict(fit.all, new_v.df, type = 'response')
-RMSE.all <- sqrt(mean((input.response - pred.response.all)^2))
-
+fit.all <- ctree(price.thousands ~ ., data = training.data.cleaned)
 fit.rsquared <- ctree(price.thousands ~ grade + sqft_living + zipcode +
-                            sqft_above + sqft_living15 + bathrooms + sqft_basement + view + 
-                            sqft_living_diff + yr_renovated + lat + bedrooms, data = new_t.df)
-pred.response.rsquare <- predict(fit.rsquared, new_v.df, type = 'response')
-RMSE.rsquared <- sqrt(mean((input.response - pred.response.rsquare)^2))
-
-fit.upper <- ctree(price.thousands ~ grade + sqft_living + zipcode + 
-                     sqft_above + sqft_living15 + bathrooms, data = new_t.df)
-pred.response.upper <- predict(fit.upper, new_v.df, type = 'response')
-RMSE.upper <- sqrt(mean((input.response - pred.response.upper)^2))
-
-fit.lower <- ctree(price.thousands ~ sqft_basement + view + sqft_living_diff + 
-                     yr_renovated + lat + bedrooms, data = new_t.df)
-pred.response.lower <- predict(fit.rsquared.all, new_v.df, type = 'response')
-RMSE.lower <- sqrt(mean((input.response - pred.response.lower)^2))
-
-
-# Plotting fit against residuals
-plot(fitted(model.2), residuals(model.2))
-
-
-
-
+                            sqft_above + sqft_living15 + bathrooms + sqft_basement + view +
+                            sqft_living_diff + yr_renovated + lat + bedrooms, data = training.data.cleaned)
+fit.upper <- ctree(price.thousands ~ grade + sqft_living + zipcode +
+                     sqft_above + sqft_living15 + bathrooms, data = training.data.cleaned)
+fit.lower <- ctree(price.thousands ~ sqft_basement + view + sqft_living_diff +
+                     yr_renovated + lat + bedrooms, data = training.data.cleaned)
