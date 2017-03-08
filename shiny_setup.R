557ffb9642e3a4f09fb7ba690487dffaee5b9fa2
Spencer Pease <spencer.p.816@gmail.com>
2017-03-08 04:37:33 -0800

    Create funtion to make a plot of a given model

--- shiny_setup.R
+++ shiny_setup.R
@@ -1,4 +1,4 @@
-## File to set up data Shiny app needs
+## File to set up datam Shiny app needs
 #
 
 # Needed libraries
@@ -12,7 +12,46 @@ pred.data <- read_csv('data/pred_data.csv') %>%
            prediction.all,
            prediction.lower,
            prediction.upper,
-           prediction.rsquare)
+           prediction.rsquare) %>%
+    mutate(price = price / 1000)
 
 model.rmse <- read_csv('data/models_rmse.csv')
+
+
+########################
+### Plot predictions ###
+########################
+
+plot_pred <- function(model) {
+
+    col.name <- paste0('prediction.', model)
+
+    g <- ggplot(data = pred.data,
+                aes(x = price),
+                aes_string(y = col.name)) +
+        geom_point(aes_string(y = col.name),
+                   color = '#0066cc',
+                   alpha = .5,
+                   size = 2) +
+        geom_smooth(aes_string(y = col.name),
+                    method = 'lm',
+                    se = FALSE,
+                    color = '#d8b365',
+                    size = 1.5) +
+        geom_abline(intercept = 0,
+                    slope = 1,
+                    size = 1.5,
+                    color = '#990033') +
+        scale_x_continuous(limits = c(0, max(pred.data$price))) +
+        scale_y_continuous(limits = c(0, max(pred.data$price))) +
+        labs(x = 'Actual price (thousands of dollars)',
+             y = 'Predicted Price (thousands of dollars)')
+
+    return(g)
+}
+
+g.all <- plot_pred('all')
+g.lower <- plot_pred('lower')
+g.upper <- plot_pred('upper')
+g.rsquare <- plot_pred('rsquare')
 
